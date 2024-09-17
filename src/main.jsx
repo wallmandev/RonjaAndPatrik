8067bfa1e7e6f7c5ba38c18d772c6610888f92a5
christofferwallman <wallman92@gmail.com>
2024-09-17 13:23:46 +0200

    added homepage

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';  // Importera BrowserRouter
+import { HashRouter as BrowserRouter } from 'react-router-dom';  // Importera BrowserRouter
 import App from './App.jsx';
 import './index.css';
 
