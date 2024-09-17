2162674a65e2dd0ae66e9fb43ca4655c9d62d13d
christofferwallman <wallman92@gmail.com>
2024-09-17 14:39:02 +0200

    a

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/RonjaAndPatrik/',  // Använd rätt bas-URL för GitHub Pages
   plugins: [react()],
 });
